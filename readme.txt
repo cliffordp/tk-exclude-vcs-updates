13f82c8ec498fad42788a9a5fc27d889b037a80d
Clifford Paulick <tko@tourkick.com>
2019-05-29 14:44:41 -0500

    Initial version

---
+++ readme.txt
@@ -0,0 +1,25 @@
+=== TK Exclude VCS Updates ===
+Contributors: cliffpaulick
+Tags: updates, vcs, git, svn, hg, mercurial
+Requires at least: 4.5
+Tested up to: 5.2.1
+Requires PHP: 5.6
+Stable tag: 1.0.0
+License: GPL version 3 or any later version
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+Exclude plugins whose folders contain a VCS directory from appearing in the list of updates.
+
+== Installation ==
+
+Install and activate like any other plugin!
+
+* You can upload the plugin zip file via the *Plugins ‣ Add New* screen
+* You can unzip the plugin and then upload to your plugin directory (typically _wp-content/plugins)_ via FTP
+* Once it has been installed or uploaded, simply visit the main plugin list and activate it
+
+== Changelog ==
+
+= [1.0.0] 2019-05-29 =
+
+* Initial release
---
+++ tk-exclude-vcs-updates.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name:       TK Exclude VCS Updates
+ * Description:       Exclude plugins whose folders contain a VCS directory from appearing in the list of updates.
+ * Version:           1.0.0
+ * GitHub Plugin URI: https://github.com/cliffordp/tk-exclude-vcs-updates
+ * Author:            TourKick LLC (Clifford Paulick)
+ * Author URI:        https://tourkick.com/
+ * License:           GPL version 3 or any later version
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       tk-exclude-vcs-updates
+ *
+ *     This plugin is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     any later version.
+ *
+ *     This plugin is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ */
+
+namespace TK_Exclude_VCS_Updates;
+
+// Abort if this file is called directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require_once( 'class-core.php' );
+require_once( 'class-plugins.php' );
+
+new Plugins();
